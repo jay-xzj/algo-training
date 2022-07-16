24763325b1c8d41e60b6ecbf2b4f9a47555950c5
jay-xzj <xzj_thrive@126.com>
2022-07-16 22:32:28 +0800

    add git ignore

--- src/main/java/com/algo/array/LC898.java
+++ src/main/java/com/algo/array/LC898.java
@@ -1,7 +1,14 @@
 package com.algo.array;
 
+import java.util.Arrays;
+
 public class LC898 {
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(shortestToChar("loveleetcode",'e')));
+    }
 
+    public static int[] shortestToChar(String s, char c) {
+        int[] arr = new int[2];
+        return arr;
     }
 }
